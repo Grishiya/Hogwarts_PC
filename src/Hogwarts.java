4442cdb9ee56713f84e0dcafe533984a19b2d16e
Grisha <guruwolf8@gmail.com>
2023-07-06 15:23:13 +0300

    Создание Хогвартса. Прописка полей,создание базовых методов.

--- src/Hogwarts.java
+++ src/Hogwarts.java
@@ -1,3 +1,46 @@
 public class Hogwarts {
+    private String name;
+    private String surname;
+    private int magicPower;
+    private int skillRange;
+
+    public Hogwarts(String name, String surname, int magicPower, int skillRange) {
+        this.name = name;
+        this.surname = surname;
+        this.magicPower = magicPower;
+        this.skillRange = skillRange;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getMagicPower() {
+        return magicPower;
+    }
+
+    public int getSkillRange() {
+        return skillRange;
+    }
+
+    public void setMagicPower(int magicPower) {
+        if (magicPower<0 && magicPower>100){
+            System.out.println("Такой силы магии не бывает");
+            return;
+        }
+        this.magicPower = magicPower;
+    }
+
+    public void setSkillRange(int skillRange) {
+        if (skillRange<0 && skillRange>100){
+            System.out.println("Невозможная дальность умений");
+            return;
+        }
+        this.skillRange = skillRange;
+    }
 
 }
